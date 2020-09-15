31e138ab8e1b433de532fa70621c4eb31d83f564
john5790 <john5790@ISS2TNPKH2.scdsb.pri>
2020-09-15 12:14:19 -0400

    Started PizzaCost1

--- src/PizzzaCost1.java
+++ src/PizzzaCost1.java
@@ -1,9 +1,14 @@
 
+import java.util.Scanner;
+
+
 public class PizzzaCost1 {
 
     
     public static void main(String[] args) {
-        
+        Scanner s = new Scanner(System.in);
+        final double labour = 1.5;
+        final double heat = 0.75;
     }
     
 }
