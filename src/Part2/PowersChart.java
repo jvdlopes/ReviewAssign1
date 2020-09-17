3d74ebf585043dcf35414fd72ee4d943e996e2d3
john5790 <john5790@ISS2TNPKH2.scdsb.pri>
2020-09-17 13:32:12 -0400

    Finished PowersChart and SalaryTable

--- src/Part2/PowersChart.java
+++ src/Part2/PowersChart.java
@@ -1,11 +1,29 @@
 package Part2;
 
+import java.util.Scanner;
 
 public class PowersChart {
 
-    
     public static void main(String[] args) {
-       
+        int num;
+        Scanner s = new Scanner(System.in);
+        while (true) {
+            System.out.print("Please give an exponent value ");
+            num = s.nextInt();
+            if(num >= 0 && num <= 10) break;
+            else System.out.println("The number must be between 0 and 10");
+        }
+
+        System.out.format("%5s", "X");
+        System.out.format("%20s\n", "2^X");
+        System.out.format("%5s", "-");
+        System.out.format("%21s\n", "-----");
+
+        for (int x = 0; x <= num; x++) {
+            System.out.format("%5s", x);
+            System.out.format("%19s\n", Math.pow(2, x));
+
+        }
     }
-    
+
 }
--- src/Part2/SalaryTable.java
+++ src/Part2/SalaryTable.java
@@ -6,7 +6,32 @@ public class SalaryTable {
 
     
     public static void main(String[] args) {
+        int raise, newsal;
+        int oldsal = 40000;
         
+        System.out.format("%5s", "YEAR");
+        System.out.format("%20s", "OLD SALARY");
+        System.out.format("%15s", "RAISE");
+        System.out.format("%20s\n", "NEW SALARY");
+        System.out.format("%5s", "----");
+        System.out.format("%20s", "----------");
+        System.out.format("%15s", "-----");
+        System.out.format("%20s\n", "----------");
+        
+            System.out.format("%5s", 1);
+            System.out.format("%20s", "---");
+            System.out.format("%15s", "$40,000");
+            System.out.format("%20s\n", "$40,000");
+            
+        for (int x = 2; x <= 10; x++) {
+            raise = (int) (0.03 * (int)oldsal);
+            newsal = oldsal + raise;
+            System.out.format("%5s", x);
+            System.out.format("%20s", "$" + oldsal);
+            System.out.format("%15s", "$" + raise);
+            System.out.format("%20s\n", "$" + newsal);
+            oldsal = newsal;
+        }
     }
     
 }
